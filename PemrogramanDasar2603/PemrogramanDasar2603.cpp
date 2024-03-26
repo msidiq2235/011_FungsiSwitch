bb0d715cbb79471f2892099825bc7c5cdccade1b
msidiq2235 <muhsidiq2005@gmail.com>
2024-03-26 17:32:52 +0700

    Add project files.

---
+++ PemrogramanDasar2603/PemrogramanDasar2603.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int bilA, bilB;
+
+void input()
+{
+	cout << "Masukan bilangan Pertama = ";
+	cin >> bilA;
+	cout << "Masukkan bilangan Kedua = ";
+	cin >> bilB;
+}
